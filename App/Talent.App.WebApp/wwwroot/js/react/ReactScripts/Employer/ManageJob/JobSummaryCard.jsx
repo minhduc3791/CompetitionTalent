1a6660fc5a85d19c5603ba82d27a3359dda83605
minhduc3791 <minhduc3791@gmail.com>
2020-07-28 19:05:09 +1000

    add list job, basic UI without function and style

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
@@ -1,6 +1,6 @@
 ﻿import React from 'react';
 import Cookies from 'js-cookie';
-import { Popup } from 'semantic-ui-react';
+import { Popup, Button, Card, Label, Icon } from 'semantic-ui-react';
 import moment from 'moment';
 
 export class JobSummaryCard extends React.Component {
@@ -15,5 +15,34 @@ export class JobSummaryCard extends React.Component {
     }
 
     render() {
+        const { title, location, noOfSuggestions, summary } = this.props;
+        return (
+            <Card>
+                <Card.Content>
+                    <Card.Header>{title}</Card.Header>
+                    <Label ribbon="right" color="black"><Icon name="user" />{noOfSuggestions}</Label>
+                    
+                    <Card.Meta>{location.city}, {location.country}</Card.Meta>
+                    <Card.Description>{summary}</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <Button color='red'>
+                        Expired
+                    </Button>
+                    <Button basic color='blue'>
+                        <Icon name="ban" />
+                        Close
+                    </Button>
+                    <Button basic color='blue'>
+                        <Icon name="edit"/>
+                        Edit
+                    </Button>
+                    <Button basic color='blue'>
+                        <Icon name="copy outline" />
+                        Copy
+                    </Button>
+                </Card.Content>
+            </Card>
+        )
     }
 }
--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
@@ -5,7 +5,7 @@ import LoggedInBanner from '../../Layout/Banner/LoggedInBanner.jsx';
 import { LoggedInNavigation } from '../../Layout/LoggedInNavigation.jsx';
 import { JobSummaryCard } from './JobSummaryCard.jsx';
 import { BodyWrapper, loaderData } from '../../Layout/BodyWrapper.jsx';
-import { Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment } from 'semantic-ui-react';
+import { Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment, Card } from 'semantic-ui-react';
 
 export default class ManageJob extends React.Component {
     constructor(props) {
@@ -43,11 +43,13 @@ export default class ManageJob extends React.Component {
         this.setState({ loaderData });//comment this
 
         //set loaderData.isLoading to false after getting data
-        //this.loadData(() =>
-        //    this.setState({ loaderData })
-        //)
+        this.loadData(() =>
+            this.setState({ loaderData }, () => {
+                console.log(this.state);
+            })
+        )
         
-        //console.log(this.state.loaderData)
+        console.log(this.state.loaderData)
     }
 
     componentDidMount() {
@@ -55,8 +57,26 @@ export default class ManageJob extends React.Component {
     };
 
     loadData(callback) {
-        var link = 'http://localhost:51689/listing/listing/getSortedEmployerJobs';
+        let queryString = `activePage=${this.state.activePage}&`;
+        queryString += Object.keys(this.state.filter)
+            .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(this.state.filter[k]))
+            .join('&');
+
+        var link = 'http://localhost:51689/listing/listing/getSortedEmployerJobs?';
         var cookies = Cookies.get('talentAuthToken');
+
+        $.ajax({
+            url: link + queryString,
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "GET",
+            success: function (res) {
+                this.setState({ loadJobs: res.myJobs });
+                callback();
+            }.bind(this)
+        })
        // your ajax call and other logic goes here
     }
 
@@ -75,9 +95,33 @@ export default class ManageJob extends React.Component {
     }
 
     render() {
+        const { activePage, totalPages } = this.state;
         return (
             <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
-               <div className ="ui container">Your table goes here</div>
+                <div className="ui container">
+                    <h1>List of Jobs</h1>
+                    <Icon name="filter" /> <p>Filter:</p>
+                    <Dropdown
+                        inline
+                        floating
+                        options={[]}
+                        search
+                        text='Choose filter'
+                    />
+                    <Icon name="calendar" /> <p>Sort by date:</p>
+                    <Dropdown
+                        inline
+                        floating
+                        options={[]}
+                        search
+                        text='Newest first'
+                    />
+                    <Card.Group>
+                        {this.state.loadJobs.length > 0 ? this.state.loadJobs.map(job => (<JobSummaryCard key={job.id} {...job} />)) : <p>No Jobs Found</p>}
+                    </Card.Group>
+
+                    <Pagination defaultActivePage={activePage} totalPages={totalPages} />
+                </div>
             </BodyWrapper>
         )
     }
