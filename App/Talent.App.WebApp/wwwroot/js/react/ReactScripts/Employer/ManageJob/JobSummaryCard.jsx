d10f8220cb17eae99efecbdbf4a80357cd84bd7e
minhduc3791 <minhduc3791@gmail.com>
2020-07-30 12:44:51 +1000

    style update add close job function

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/JobSummaryCard.jsx
@@ -6,7 +6,11 @@ import moment from 'moment';
 export class JobSummaryCard extends React.Component {
     constructor(props) {
         super(props);
-        this.selectJob = this.selectJob.bind(this)
+        this.state = {
+            isLoading: false,
+        }
+        this.selectJob = this.selectJob.bind(this);
+        this.closeJob = this.closeJob.bind(this);
     }
 
     selectJob(id) {
@@ -14,33 +18,61 @@ export class JobSummaryCard extends React.Component {
         //url: 'http://localhost:51689/listing/listing/closeJob',
     }
 
+    closeJob(jobId) {
+        this.setState({ isLoading: true });
+        var link = 'http://localhost:51689/listing/listing/closeJob';
+        var cookies = Cookies.get('talentAuthToken');
+
+        $.ajax({
+            url: link,
+            headers: {
+                'Authorization': 'Bearer ' + cookies,
+                'Content-Type': 'application/json'
+            },
+            type: "POST",
+            contentType: "application/json",
+            dataType: "json",
+            data: JSON.stringify(jobId),
+            success: function (res) {
+                console.log(res);
+                this.props.updateWithoutSave(jobId);
+            }.bind(this),
+            error: function (res) {
+                this.setState({ isLoading: false });
+                TalentUtil.notification.show("Error while closing job", "error", null, null);
+            }.bind(this)
+        })
+    }
+
     render() {
-        const { title, location, noOfSuggestions, summary } = this.props;
+        const { title, location, noOfSuggestions, summary, id } = this.props;
+        const { isLoading } = this.state;
         return (
-            <Card>
+            <Card as='a' className="job-summary-card">
                 <Card.Content>
                     <Card.Header>{title}</Card.Header>
                     <Label ribbon="right" color="black"><Icon name="user" />{noOfSuggestions}</Label>
-                    
-                    <Card.Meta>{location.city}, {location.country}</Card.Meta>
-                    <Card.Description>{summary}</Card.Description>
+                    <Card.Meta>{location.city} {location.city && location.city.length > 0 && ' ,'} {location.country}</Card.Meta>
+                    <Card.Description className="job-summary">{summary}</Card.Description>
                 </Card.Content>
-                <Card.Content extra>
-                    <Button color='red'>
+                <Card.Content extra className="center">
+                    <Button color='red' size='mini' floated="left">
                         Expired
                     </Button>
-                    <Button basic color='blue'>
-                        <Icon name="ban" />
-                        Close
-                    </Button>
-                    <Button basic color='blue'>
-                        <Icon name="edit"/>
-                        Edit
-                    </Button>
-                    <Button basic color='blue'>
-                        <Icon name="copy outline" />
-                        Copy
-                    </Button>
+                    <Button.Group size="mini" floated="right">
+                        <Button basic color='blue' disabled={isLoading} onClick={() => this.closeJob(id)}>
+                            <Icon name="ban" />
+                            Close
+                        </Button>
+                        <Button basic color='blue'>
+                            <Icon name="edit" />
+                            Edit
+                        </Button>
+                        <Button basic color='blue'>
+                            <Icon name="copy outline" />
+                            Copy
+                        </Button>
+                    </Button.Group>
                 </Card.Content>
             </Card>
         )
--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/Employer/ManageJob/ManageJob.jsx
@@ -5,7 +5,9 @@ import LoggedInBanner from '../../Layout/Banner/LoggedInBanner.jsx';
 import { LoggedInNavigation } from '../../Layout/LoggedInNavigation.jsx';
 import { JobSummaryCard } from './JobSummaryCard.jsx';
 import { BodyWrapper, loaderData } from '../../Layout/BodyWrapper.jsx';
-import { Pagination, Icon, Dropdown, Checkbox, Accordion, Form, Segment, Card } from 'semantic-ui-react';
+import { Pagination, Icon, Dropdown, Grid, Header, Card, Divider } from 'semantic-ui-react';
+
+const PAGE_SIZE = 6;
 
 export default class ManageJob extends React.Component {
     constructor(props) {
@@ -34,6 +36,7 @@ export default class ManageJob extends React.Component {
         this.loadData = this.loadData.bind(this);
         this.init = this.init.bind(this);
         this.loadNewData = this.loadNewData.bind(this);
+        this.updateWithoutSave = this.updateWithoutSave.bind(this);
         //your functions go here
     };
 
@@ -44,12 +47,8 @@ export default class ManageJob extends React.Component {
 
         //set loaderData.isLoading to false after getting data
         this.loadData(() =>
-            this.setState({ loaderData }, () => {
-                console.log(this.state);
-            })
+            this.setState({ loaderData })
         )
-        
-        console.log(this.state.loaderData)
     }
 
     componentDidMount() {
@@ -57,7 +56,7 @@ export default class ManageJob extends React.Component {
     };
 
     loadData(callback) {
-        let queryString = `activePage=${this.state.activePage}&`;
+        let queryString = `activePage=${this.state.activePage}&sortbyDate=${this.state.sortBy.date}&`;
         queryString += Object.keys(this.state.filter)
             .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(this.state.filter[k]))
             .join('&');
@@ -73,11 +72,21 @@ export default class ManageJob extends React.Component {
             },
             type: "GET",
             success: function (res) {
-                this.setState({ loadJobs: res.myJobs });
+                this.setState({
+                    loadJobs: res.myJobs,
+                    totalPages: res.totalCount / PAGE_SIZE
+                });
                 callback();
             }.bind(this)
         })
        // your ajax call and other logic goes here
+    }
+
+    updateWithoutSave(jobId) {
+        this.setState(prevState => ({
+            loadJobs: prevState.loadJobs.filter(job => job.id !== jobId),
+            isLoading: false,
+        }))
     }
 
     loadNewData(data) {
@@ -95,32 +104,36 @@ export default class ManageJob extends React.Component {
     }
 
     render() {
-        const { activePage, totalPages } = this.state;
+        const { activePage, totalPages, loadJobs } = this.state;
         return (
             <BodyWrapper reload={this.init} loaderData={this.state.loaderData}>
-                <div className="ui container">
-                    <h1>List of Jobs</h1>
-                    <Icon name="filter" /> <p>Filter:</p>
-                    <Dropdown
-                        inline
-                        floating
-                        options={[]}
-                        search
-                        text='Choose filter'
-                    />
-                    <Icon name="calendar" /> <p>Sort by date:</p>
-                    <Dropdown
-                        inline
-                        floating
-                        options={[]}
-                        search
-                        text='Newest first'
-                    />
-                    <Card.Group>
-                        {this.state.loadJobs.length > 0 ? this.state.loadJobs.map(job => (<JobSummaryCard key={job.id} {...job} />)) : <p>No Jobs Found</p>}
-                    </Card.Group>
-
-                    <Pagination defaultActivePage={activePage} totalPages={totalPages} />
+                <div className="ui container manage-jobs">
+                    <Header size="large">List of Jobs</Header>
+                    <div className="jobs-content">
+                        <Grid column={2}>
+                            <Icon name="filter" />Filter:
+                            <Dropdown
+                                inline
+                                floating
+                                options={[]}
+                                search
+                                text='Choose filter'
+                            />
+                            <Icon name="calendar" />Sort by date:
+                            <Dropdown
+                                inline
+                                floating
+                                options={[]}
+                                search
+                                text='Newest first'
+                            />
+                        </Grid>
+                        {(!loadJobs.length || loadJobs.length === 0) && <Grid><p>No Jobs Found</p></Grid>}
+                        <Card.Group className="jobs-list" itemsPerRow={3}>
+                            {loadJobs && loadJobs.length > 0 && loadJobs.map(job => (<JobSummaryCard updateWithoutSave={this.updateWithoutSave} key={job.id} {...job} />))}
+                        </Card.Group>
+                        <Pagination floated="right" defaultActivePage={activePage} totalPages={totalPages} />
+                    </div>
                 </div>
             </BodyWrapper>
         )
